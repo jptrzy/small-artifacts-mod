02929b131316ef0043a14a3ca229b747c66c6034
jptrzy <jptrzy@gmail.com>
2021-09-21 18:00:05 +0200

    Add copper altar

---
+++ src/main/java/net/jptrzy/small/artifacts/Blocks/CopperAltar.java
@@ -0,0 +1,11 @@
+package net.jptrzy.small.artifacts.Blocks;
+
+import net.minecraft.block.Block;
+
+public class CopperAltar extends Block {
+
+    public CopperAltar(Settings settings) {
+        super(settings);
+    }
+
+}
